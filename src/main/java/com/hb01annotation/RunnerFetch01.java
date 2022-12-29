063bb62a8dc5ffdc99f210cc8f3fda399822107d
speculumoris <sefa.byndr4@gmail.com>
2022-12-29 12:29:37 +0300

    thirdLesson

--- src/main/java/com/hb01annotation/RunnerFetch01.java
+++ src/main/java/com/hb01annotation/RunnerFetch01.java
@@ -5,7 +5,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,7 +25,9 @@ public class RunnerFetch01 {
         System.out.println(student3);
 
       */
-       /* String sqlQuery ="SELECT * FROM t_student01 order by id";
+       /*
+        2. yol  SQL
+        String sqlQuery ="SELECT * FROM t_student01 order by id";
         List<Object[]> resultList= session.createSQLQuery(sqlQuery).getResultList();
 
         for (Object [] object:resultList){
@@ -42,7 +43,8 @@ public class RunnerFetch01 {
             System.out.println(student);
         }
 
-        */
+
+        //4.yol
         String sqlQuery2="SELECT * FROM t_student01 WHERE student_name='Yusuf Bay'";
         Object[] uniqueResult1= (Object[]) session.createSQLQuery(sqlQuery2).uniqueResult();
         System.out.println(Arrays.toString(uniqueResult1));
@@ -50,6 +52,21 @@ public class RunnerFetch01 {
         String hqlQuery2="FROM Student01 WHERE student_name='Yusuf Bay2'";
         Student01 uniqueResult2= session.createQuery(hqlQuery2,Student01.class).uniqueResult();
         System.out.println(uniqueResult2);
+
+        */
+        //yukarıdaki sorguyu HQL Alias kullanarak yapalım
+        String hqlQuery3 ="FROM Student01 std WHERE std.name='Yusuf Bay'";
+        Student01 uniqueResult3=session.createQuery(hqlQuery3,Student01.class).uniqueResult();
+        System.out.println(uniqueResult3);
+
+        String hqlQuery4="SELECT s.id,s.name FROM Student01 WHERE s.grade=90";
+        List<Object[]>uniqueResult4=session.createQuery(hqlQuery4).getResultList();
+        // createQuery metoduna tek parametre girdiğimiz için Student01 clası ile mappleme işlemi yapılmadı
+        // bu yüzden Object olarak aldık
+        for (Object[] w:uniqueResult4) {
+            System.out.println(Arrays.toString(w));
+        }
+
 
         tx.commit();
         session.close();
---
+++ src/main/java/com/hb02/ebeddable/RunnerSave02.java
@@ -0,0 +1,52 @@
+package com.hb02.ebeddable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class RunnerSave02 {
+
+
+    public static void main(String[] args) {
+        Student02 student1 = new Student02();
+        student1.setId(1001);
+        student1.setName("Daffy Kabukcu");
+        student1.setGrade(90);
+
+        Address address1 = new Address();
+        address1.setStreet("Apple Street");
+        address1.setCity("NewYork");
+        address1.setCountry("US");
+        address1.setZipCode("06852");
+
+        student1.setAdress(address1);
+
+        Address address2 = new Address();
+        address2.setStreet("Orange Street");
+        address2.setCity("London");
+        address2.setCountry("England");
+        address2.setZipCode("03452");
+
+        Student02 student2 = new Student02();
+        student2.setId(1002);
+        student2.setName("Beyza Keser");
+        student2.setGrade(90);
+        student2.setAdress(address2);
+
+        Configuration con=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student02.class);
+
+        SessionFactory sf= con.buildSessionFactory();
+        Session session=sf.openSession();
+        Transaction tx=session.beginTransaction();
+
+        session.save(student1);
+        session.save(student2);
+
+        tx.commit();
+        session.close();
+        sf.close();
+
+    }
+
+}
